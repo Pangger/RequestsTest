fbcdc3149d2e3d2e67d8a1c80aa31e238b28c3b3
PC-105400\Maksym <m.laktionov@mobidev.biz>
2021-04-22 16:52:25 +0300

    Add project files.

---
+++ LinksReader.cs
@@ -0,0 +1,116 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RequestsTest
+{
+    public class LinksReader
+    {
+        public string WebAddress { get; set; }
+
+        public IConfiguration Cfg { get; set; }
+
+        public LinksReader()
+        {
+            WebAddress = "google.com";
+            Cfg = Configuration.Default.WithDefaultLoader();
+        }
+
+
+        public async Task<List<string>> HtmlReadLinks()
+        {
+            var linksList = new List<string>();
+            var document = await BrowsingContext.New(Cfg).OpenAsync($"https://{WebAddress}");
+            var links = document.QuerySelectorAll("a");
+            foreach (var item in links)
+            {
+                var link = item.GetAttribute("href");
+                if (link.Contains("http://") || link.Contains("https://"))
+                {
+                    linksList.Add(link);
+                }
+                else
+                {
+                    linksList.Add("http://" + WebAddress + link);
+                }
+            }
+            return linksList;
+        }
+        public async Task<List<string>> XmlReadLinks()
+        {
+
+            var documentSiteMap = await BrowsingContext.New(Cfg).OpenAsync($"https://{WebAddress}/sitemap.xml");
+
+            var links = documentSiteMap.QuerySelectorAll("loc");
+
+            var linksList = new List<string>();
+            foreach (var link in links)
+            {
+                linksList.Add(link.ToHtml()
+                    .Replace("<loc>", "")
+                    .Replace("</loc>", "")
+                    .Replace($"https://{WebAddress}/sitemap.xml", ""));
+            }
+            if (linksList.Count == 0)
+            {
+                return null;
+            }
+
+            return linksList;
+        }
+
+
+        public void ElapseTime(List<string> links)
+        {
+
+            using (var client = new WebClient())
+            {
+
+                foreach (var link in links)
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+
+                    stopwatch.Start();
+                    var result = client.DownloadString(link);
+                    stopwatch.Stop();
+                    Console.WriteLine(link + " = " + stopwatch.Elapsed);
+                }
+
+                Console.WriteLine("Elapsing is finished");
+            }
+        }
+
+        public void Compare(List<string> linksHtml, List<string> linksXml)
+        {
+            if (linksXml != null)
+            {
+                for (int i = 0; i < linksXml.Count; i++)
+                {
+                    for (int j = 0; j < linksHtml.Count; j++)
+                    {
+                        if (linksXml[i] == linksHtml[j])
+                        {
+                            Console.WriteLine(linksXml[i]);
+                        }
+                    }
+                }
+
+                Console.WriteLine("Comparing is finished");
+            }
+            else
+            {
+                Console.WriteLine("Sitemap xml is empty");
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RequestsTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var TestLinks = new LinksReader();
+            TestLinks.WebAddress = Console.ReadLine();
+            var xmlLinks = TestLinks.XmlReadLinks();
+            var htmlLinks = TestLinks.HtmlReadLinks();
+
+            TestLinks.Compare(htmlLinks.Result, xmlLinks.Result);
+            TestLinks.ElapseTime(htmlLinks.Result);
+            TestLinks.ElapseTime(xmlLinks.Result);
+
+            Console.ReadKey();
+        }
+    }
+}
